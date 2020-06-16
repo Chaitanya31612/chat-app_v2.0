0e7cdb2ef3c14df0d20e3c70ad7c46776118ed2f
Chaitanya <guptachaitanya31@gmail.com>
2020-06-17 00:03:39 +0530

    init commit

---
+++ src/utils/messages.js
@@ -0,0 +1,21 @@
+const generateMessage = (username, text) => {
+    if (text !== "") {
+        return {
+            username,
+            text,
+            createdAt: new Date().getTime()
+        }
+    }
+}
+
+// const generateLocationMessage = (username, url) => {
+//     return {
+//         username,
+//         url,
+//         createdAt: new Date().getTime()
+//     }
+// }
+
+module.exports = {
+    generateMessage
+}
